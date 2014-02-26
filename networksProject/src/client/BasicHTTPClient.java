1e07ae91d922ede33a9886d0bc3c8f4c8f57096c
qqsmalleindigoqq <wa_waxd@msn.com>
2014-02-26 14:43:46 +0100

    client

--- networksProject/src/client/BasicHTTPClient.java
+++ networksProject/src/client/BasicHTTPClient.java
@@ -1,8 +1,27 @@
 package client;
 
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class BasicHTTPClient {
 	
+	Socket sock = null;
 	
+	public void connect(int port) throws UnknownHostException, IOException{
+		sock = new Socket("localhost",port);
+	}
+
+	public void sendMessage(String message) throws IOException {
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
+		writer.write(message);
+		writer.flush();
+		
+		
+	}
 	
 
 }
---
+++ networksProject/src/client/Main.java
@@ -0,0 +1,31 @@
+package client;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Main {
+		
+		public static void main(String[] args) {
+			BasicHTTPClient bhc= new BasicHTTPClient();
+			Scanner scan = new Scanner(System.in);
+			System.out.println("which port?");
+			int port;
+			port = scan.nextInt();
+			
+			try {
+				bhc.connect(port);
+				System.out.println("Connection made");
+				System.out.println("What message would you like to send?");
+				String message =scan.next();
+				bhc.sendMessage(message);
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+}
