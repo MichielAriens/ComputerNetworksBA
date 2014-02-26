bad885274182d6a8b0d3eff82b37dbe283f2be61
Michiel Ariens <michiel.ariens@student.kuleuven.be>
2014-02-26 14:31:49 +0100

    server reply

--- networksProject/src/server/RequestHandler.java
+++ networksProject/src/server/RequestHandler.java
@@ -1,10 +1,13 @@
 package server;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.net.Socket;
 
@@ -22,8 +25,11 @@ public class RequestHandler implements Runnable {
 			InputStream inStream = soc.getInputStream();
 			BufferedReader reader = new BufferedReader(new InputStreamReader(inStream));
 			String s = reader.readLine();
-			DataOutputStream 
-			
+			OutputStream outStream = soc.getOutputStream();
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
+			writer.write("Server recived: " + s);
+			writer.flush();
+
 			
 			System.out.println(s);
 			
