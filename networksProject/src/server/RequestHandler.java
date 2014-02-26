9b297da82cc192bb6c32d79cb56e804a68866b61
qqsmalleindigoqq <wa_waxd@msn.com>
2014-02-26 15:40:08 +0100

    stringsplitser

--- networksProject/src/server/RequestHandler.java
+++ networksProject/src/server/RequestHandler.java
@@ -2,37 +2,34 @@ package server;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.Reader;
 import java.net.Socket;
 
 public class RequestHandler implements Runnable {
 	
 	private final Socket soc;
-	
+	private ProcessCommand proccomm;
 	public RequestHandler(Socket soc) {
+		proccomm = new ProcessCommand();
 		this.soc = soc;
 	}
 
 	@Override
 	public void run() {
 		try {
-			while(true){
+			
 				BufferedReader reader = new BufferedReader(new InputStreamReader(soc.getInputStream()));
 				String s = reader.readLine();
 				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(soc.getOutputStream()));
 				writer.write("Server recived: " + s);
 				writer.flush();
-	
+				proccomm.analyseCommand(s);
 				
 				System.out.println(s);
 				
-			}
+			
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
